a8881f34d453086e10f0530eba736f33c7eeba43
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-27 13:53:13 +0200

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  title: "Lindiwe's Portfolio",
+  name: "Lindiwe",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
+  twitter: "https://x.com/ThabsileDl",
   github: "https://github.com/1msirius/Nextfolio",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://www.linkedin.com/in/lindiwe-dlomo-b50050b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=and",
+  email: "mailto: lindiwethaby@hotmail.com",
 };
