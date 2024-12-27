1c7c1b6f3c823a6338eb1681e22d8f2f9c30e335
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-27 14:02:08 +0200

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -9,7 +9,7 @@ export const metaData = {
 
 export const socialLinks = {
   twitter: "https://x.com/ThabsileDl",
-  github: "https://github.com/1msirius/Nextfolio",
+  github: "https://github.com/LindiweD-Collab",
   instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/in/lindiwe-dlomo-b50050b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=and",
   email: "mailto: lindiwethaby@hotmail.com",
