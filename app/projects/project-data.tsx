cd3dd9fd74ed7f8544d4d2775b7b7f717073c5a1
LindiweD-Collab <lindiwethaby96@gmail.com>
2025-01-12 11:50:55 +0200

    Update project-data.tsx

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -8,36 +8,19 @@ export interface Project {
 export const projects: Project[] = [
   {
     title: "Resume Builder",
-    year: 2023,
-    description: "*Introducing the Ultimate Resume Builder*
-Create a professional and effective resume in minutes with our intuitive and user-friendly resume builder. Our tool is designed to help you showcase your skills, experience, and achievements in a clear and concise manner, increasing your chances of landing your dream job.
-
-*Features:*
-
-- *Easy-to-use interface*: Our resume builder is designed to be simple and easy to use, even for those with little to no experience with resume writing.
-- *Pre-built templates*: Choose from a variety of professionally designed templates to give your resume a polished and modern look.
-- *Customization options*: Tailor your resume to your specific needs and preferences with our extensive range of customization options, including font styles, colors, and layouts.
-- *Keyword suggestions*: Get suggestions for relevant keywords and phrases to include in your resume, helping you pass through applicant tracking systems (ATS) and catch the eye of hiring managers.
-- *Real-time preview*: See how your resume will look as you build it, with our real-time preview feature.
-- *Download and share*: Easily download your resume in a variety of formats, including PDF, Word, and Text, and share it with others via email or social media.
-
-Try our resume builder today and take the first step towards creating a professional and effective resume that will help you land your dream job.",
+    year: 2024,
+    description: "Create a professional and effective resume in minutes with our intuitive and user-friendly resume builder. Our tool is designed to help you showcase your skills, experience, and achievements in a clear and concise manner, increasing your chances of landing your dream job.",
     url: "https://lindiwedlomo.github.io/Resume_Builder/",
   },
   {
     title: "Drum Machine",
-    year: 2022,
-    description:
-      "Here's a short draft description for a drum machine:
-
-*Introducing the BeatMaster Drum Machine*
-
-Create professional-sounding drum tracks with ease using our intuitive and powerful drum machine. With a vast library of sounds, patterns, and effects, you can produce high-quality drum beats for any genre of music.",
+    year: 2024,
+    description: "Create professional-sounding drum tracks with ease using our intuitive and powerful drum machine. With a vast library of sounds, patterns, and effects, you can produce high-quality drum beats for any genre of music.",
     url: "https://fccdrum-machine.vercel.app/",
   },
   {
     title: "VPN App",
-    year: 2021,
+    year: 2024,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
     url: "https://example.com/",
