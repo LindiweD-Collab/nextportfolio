c2488fa73635b68e726e66b075ac8aa4ea315e2f
LindiweD-Collab <lindiwethaby96@gmail.com>
2025-01-12 12:54:17 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
           <a
             href="/resume.pdf"
             download="Lindiwe-Thabsile-Dlomo-Resume.pdf"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
             Download Resume
           </a>
