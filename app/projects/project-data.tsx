462925d28bdda567484158e74a4c4a7420b8779d
LindiweD-Collab <lindiwethaby96@gmail.com>
2025-01-12 08:18:54 +0200

    Update project-data.tsx

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -9,8 +9,19 @@ export const projects: Project[] = [
   {
     title: "Resume Builder",
     year: 2023,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+    description: "*Introducing the Ultimate Resume Builder*
+Create a professional and effective resume in minutes with our intuitive and user-friendly resume builder. Our tool is designed to help you showcase your skills, experience, and achievements in a clear and concise manner, increasing your chances of landing your dream job.
+
+*Features:*
+
+- *Easy-to-use interface*: Our resume builder is designed to be simple and easy to use, even for those with little to no experience with resume writing.
+- *Pre-built templates*: Choose from a variety of professionally designed templates to give your resume a polished and modern look.
+- *Customization options*: Tailor your resume to your specific needs and preferences with our extensive range of customization options, including font styles, colors, and layouts.
+- *Keyword suggestions*: Get suggestions for relevant keywords and phrases to include in your resume, helping you pass through applicant tracking systems (ATS) and catch the eye of hiring managers.
+- *Real-time preview*: See how your resume will look as you build it, with our real-time preview feature.
+- *Download and share*: Easily download your resume in a variety of formats, including PDF, Word, and Text, and share it with others via email or social media.
+
+Try our resume builder today and take the first step towards creating a professional and effective resume that will help you land your dream job.",
     url: "https://lindiwedlomo.github.io/Resume_Builder/",
   },
   {
