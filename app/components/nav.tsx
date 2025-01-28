6b808cf8582628feedb16877120b3ca1cad463f6
LindiweD-Collab <lindiwethaby96@gmail.com>
2025-01-28 11:39:16 +0200

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,6 +3,8 @@ import { ThemeSwitch } from "./theme-switch";
 import { metaData } from "../config";
 
 const navItems = {
+    "/home": { name: "Home" },
+
   "/blog": { name: "Blog" },
   "/projects": { name: "Projects" },
   "/photos": { name: "Education" },
